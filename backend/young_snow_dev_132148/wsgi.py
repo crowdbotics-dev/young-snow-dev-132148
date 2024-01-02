b6c7a91c002f27c1bad357dc21174a3d57435504
Crowdbotics <engineering@crowdbotics.com>
2024-01-02 21:58:36 +0000

    Initial commit

---
+++ backend/young_snow_dev_132148/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for young_snow_dev_132148 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "young_snow_dev_132148.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "young-snow-dev-132148",
+  url: "https://young-snow-dev-132148.botics.co",
+  api: "https://young-snow-dev-132148.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
